b5bc403b8f13cee2dac82210973091c68cb8e5ad
Mick Schroeder <mschroeder@gmail.com>
2010-12-22 13:07:49 -0500

    Updated high-resolution toolbar icons for Retina Display

--- Resources/parse/parse.php
+++ Resources/parse/parse.php
@@ -1,30 +1,24 @@
 <?php
 
+$origLink = $_GET['Link'];
 
-function replace_content_inside_delimiters($start, $end, $new, $source) {
-return preg_replace('#('.preg_quote($start).')(.*)('.preg_quote($end).')#si', '$1'.$new.'$3', $source);
-}
-
-$doc = new DOMDocument();
-  $doc->load('http://www.ashp.org/rss/shortages');
-  $arrFeeds = array();
-  foreach ($doc->getElementsByTagName('item') as $node) {
-    $itemRSS = array ( 
-      'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
-      'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
-      );
-    array_push($arrFeeds, $itemRSS);
-  }
-
-//print_r($arrFeeds);
-
-foreach ($arrFeeds as $k) {
-$origLink = $k[link];
-
+if ($origLink) {
 $path = "http://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20html%20WHERE%20url%3D%22$origLink%22%20AND%20xpath%3D%22%2F%2Fdiv%5B%40class%3D'Center'%5D%22";
-$feed = file_get_contents($path);
+//$feed = file_get_contents($path);
+$userAgent = 'Googlebot/2.1 (http://www.googlebot.com/bot.html)';
 
-// Strip 
+// make the cURL request to 
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
+curl_setopt($ch, CURLOPT_URL,$path);
+curl_setopt($ch, CURLOPT_FAILONERROR, true);
+curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
+$feed= curl_exec($ch);
+// Strip Links
 $feed = str_replace('</a>', '', $feed);
 $feed = str_replace('<br class="clear"/>', '', $feed);
 $feed = str_replace('<br/>', '', $feed);
@@ -32,27 +26,22 @@ $feed = str_replace('<p>Back to Drug Shortage Product Bulletins</p>', '', $feed)
 $feed = preg_replace('/<a[^>]+href[^>]+>/', '', $feed);
 $feed = str_replace('<p>Back to Drug Shortage', '', $feed);
 $feed = str_replace('Product Bulletins</p>', '', $feed);
+function replace_content_inside_delimiters($start, $end, $new, $source) {
+return preg_replace('#('.preg_quote($start).')(.*)('.preg_quote($end).')#si', '$1'.$new.'$3', $source);
+}
 $feed = replace_content_inside_delimiters('<!-- Begin Related Shortages -->', '<!-- End Related Shortages -->', '', $feed);
 $feed = replace_content_inside_delimiters('<!-- Begin References -->', '<!-- End References -->', '', $feed);
+}
 
-
-$cache = $k[title];
-$cachefile = fopen($cache, 'wb');
- fwrite($cachefile, $feed);
- fclose($cachefile);
-echo $cache;
-echo "\n";
-
-sleep(1);
-
+else{
+$feed = "ERROR";
 }
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
-	<style type="text/\css">
+	<style type="text/css">
 		body { font-family: sans-serif; }
 		h1 {font-size: 1.5em;}
 	</style>
@@ -61,7 +50,18 @@ sleep(1);
 </head>
 <body> 
 <?php echo $feed; ?>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-3312121-3']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </body>
 </html>
-
-
