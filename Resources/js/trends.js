748e0af5b6bfbb61f887bb075021f7dca0e0cbce
Mick Schroeder <mschroeder@gmail.com>
2011-04-06 08:24:20 -0400

    moving closer to 2.0

--- Resources/js/trends.js
+++ Resources/js/trends.js
@@ -1,132 +1,149 @@
+//
+// Show Feed
+//
 
-// Displays a list of the recent articles 
-var win = Ti.UI.currentWindow,
-	data, newRow, siteUrl;
+// Set properties
+var win = Ti.UI.currentWindow;
 win.backgroundColor = '#fff';
 
-
-var actInd = Titanium.UI.createActivityIndicator({
-	zIndex: 1,
-	top: 'auto',
-	height: 100,
-	width: 210,
-	color: 'black',
-	font: {
-		fontFamily: 'Helvetica Neue',
-		fontSize: 15,
-		fontWeight: 'bold'
-	},
-	message: 'Loading...',
-	style: Titanium.UI.iPhone.ActivityIndicatorStyle.DARK
-});
-
-win.add(actInd);
-
+// Set variables
+var data, newRow, siteUrl;
 
 
 // Create Tableview
-var tableView = Ti.UI.createTableView({});
+var tableView = Ti.UI.createTableView({
 
-if (Ti.Network.online) {
+});
 
-	function setTableData() {
-			
-			var getJSON = Ti.Network.createHTTPClient();
-					var tableData = [];  
-			getJSON.open('GET', 'http://mickschroeder.com/pharmacy/rxshortages/trends/example.filter.php');  
-			getJSON.send();  
-			getJSON.onload = function(){  
-				var data = JSON.parse(this.responseText);  
+// Populate a tableview with the titles
+Ti.UI.currentWindow.add(tableView);
 
-				// For each item from the total number of postings returned from the query...
-					for (var i = 0, j = data.length; i < j; i++) {						
-						newRow = Ti.UI.createTableViewRow({
-							theTitle: data[i][1],
-							path: 'article.js',
-							url: data[i][0],
-							hasChild: true,
-							className: 'drug_row'
-						});
-						// Need label in order to change the font size.
-						var articleTitleLabel = Ti.UI.createLabel({
-						text: i+1 +'. '+ data[i][1],
-						left: 10,
-						right: 30,
-						font: {
-							fontSize: 15,
-							fontWeight: 'bold'
-							}
+// Refresh buttom
+var refresh = Titanium.UI.createButton({
+	systemButton: Titanium.UI.iPhone.SystemButton.REFRESH
+});
 
-						});
-						
-						newRow.add(articleTitleLabel);
-						tableData.push(newRow);
-					}
-					
-			// end YQL
-			tableView.setData(tableData);
-	}
+if (Ti.Platform.name == 'iPhone OS') {
+	win.rightNavButton = refresh;
+} 
+else {
+
 }
-	
-	// Query
-	setTableData();
-	
-	// Populate a tableview with the titles
-	Ti.UI.currentWindow.add(tableView);
 
-	// Data has been loaded/added, so remove the loading icon.
-		if(navActInd){
-		navActInd.hide();
-	}
+refresh.addEventListener('click', function () {
+	Ti.API.log('refreshing');
+	Ti.App.fireEvent("show_indicator");
+	tableView.setData(null);
+	setTableData();
 	Ti.App.fireEvent("hide_indicator");
-	
-	
-	// When a title is clicked, open a new window and pass the details of the selected posting.
-	tableView.addEventListener('click', function (e) {
-		if (e.rowData.path) {
+});
+
+
+// Choose the correct feed
+if (Ti.App.Properties.getString('websiteName') == 'Current Shortages') {
+	siteUrl = "http://www.ashp.org/rss/shortages/";
+} else if (Ti.App.Properties.getString('websiteName') == 'Resolved Shortages') {
+	siteUrl = "http://www.ashp.org/rss/resolved/";
+} else if (Ti.App.Properties.getString('websiteName') == 'Unavailable Drugs') {
+	siteUrl = "http://www.ashp.org/rss/notavailable/";
+}
+
+
+//YQL Feed
+function setTableData() {
+
+	var getJSON = Ti.Network.createHTTPClient();
+			var tableData = [];  
+	getJSON.open('GET', 'http://mickschroeder.com/pharmacy/rxshortages/trends/example.filter.php');  
+	getJSON.send();  
+	getJSON.onload = function(){  
+		var data = JSON.parse(this.responseText);  
+
+		// For each item from the total number of postings returned from the query...
+			for (var i = 0, j = data.length; i < j; i++) {						
+				newRow = Ti.UI.createTableViewRow({
+					theTitle: data[i][1],
+					path: 'article.js',
+					url: data[i][0],
+					hasChild: true,
+					className: 'drug_row'
+				});
+				// Need label in order to change the font size.
+				var articleTitleLabel = Ti.UI.createLabel({
+				text: i+1 +'. '+ data[i][1],
+				left: 10,
+				right: 30,
+				font: {
+					fontSize: 15,
+					fontWeight: 'bold'
+					}
+
+				});
+				
+				newRow.add(articleTitleLabel);
+				tableData.push(newRow);
+			}
+					tableView.setData(tableData);
+				}
+		}
+
+		// When a title is clicked, open a new window and pass the details of the selected posting.
+		tableView.addEventListener('click', function (e) {
 			var newWin = Ti.UI.createWindow({
-				url: e.rowData.path,
+				url: 'article.js',
 				//title : e.rowData.theTitle,
 				barColor: Ti.UI.currentWindow.barColor
 			});
 
 			// Add variables for the description and the url.
-			newWin.desc = e.rowData.desc;
 			newWin.theUrl = e.rowData.url;
-		}
+			newWin.pubDate = escape(e.rowData.pubDate);
 
-		Ti.UI.currentTab.open(newWin, {
-			animated: true
+			Ti.UI.currentTab.open(newWin, {
+				animated: true
+			});
+
 		});
-
-	});
-
-	var refresh = Titanium.UI.createButton({
-		systemButton: Titanium.UI.iPhone.SystemButton.REFRESH
-	});
-
-	if (Ti.Platform.name == 'iPhone OS') {
-		win.rightNavButton = refresh;
-	} else {
-
-	}
-
-	refresh.addEventListener('click', function () {
-		Ti.API.log('refreshing');
 		
-		tableView.setData(null);
-		setTableData();
-			if(navActInd){
-		navActInd.hide();
-	}
-	Ti.App.fireEvent("hide_indicator");
-	});
+		var flexSpace = Titanium.UI.createButton({
+			systemButton:Titanium.UI.iPhone.SystemButton.FLEXIBLE_SPACE
+		});
+		var bb2 = Titanium.UI.createButtonBar({
+			labels:['Day', 'Week', 'Month'],
+			//backgroundColor:'maroon'
+		});
+		
+		var toolbar2 = Titanium.UI.createToolbar({
+			items:[flexSpace,bb2,flexSpace],
+			top:0,
+			borderTop:false,
+			borderBottom:true,
+			//barColor:'#336699'
+		});
+		
+		win.add(toolbar2);
+		
+		// Make sure device is connection to the internet
 
-} else {
-	var alertDialog = Titanium.UI.createAlertDialog({
-		title: 'CONNECTION REQUIRED!',
-		message: 'Your device is not online.',
-		buttonNames: ['OK']
-	});
-	alertDialog.show();
-}
+		if (Ti.Network.online) {
+
+			// Show loading indicator 
+			Ti.App.fireEvent("show_indicator");
+
+			// Load table
+			setTableData();
+
+			// Data has been loaded/added, so remove the loading icon.
+			Ti.App.fireEvent("hide_indicator");
+
+		} 
+
+		// If not connected, alert the user
+		else {
+			var alertDialog = Titanium.UI.createAlertDialog({
+				title: 'Internet Connection Required',
+				message: 'Your device is not online',
+				buttonNames: ['OK']
+			});
+			alertDialog.show();
+		}
